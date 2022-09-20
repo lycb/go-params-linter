b6cc7f006d6c44eb8410a55cd405d544093f243f
Jenny Ly <lycb@github.com>
2022-09-20 15:12:59 -0400

    add ast inspector and tests

--- pkg/analyzer/analyzer.go
+++ pkg/analyzer/analyzer.go
@@ -1,24 +1,31 @@
 package analyzer
 
 import (
-	"fmt"
 	"go/ast"
 
 	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/passes/inspect"
+	"golang.org/x/tools/go/ast/inspector"
 )
 
 var Analyzer = &analysis.Analyzer{
-	Name: "goparamslinter",
-	Doc:  "Check if params have the same type",
-	Run:  run,
+	Name:     "goparamslinter",
+	Doc:      "Check if params have the same type",
+	Run:      run,
+	Requires: []*analysis.Analyzer{inspect.Analyzer},
 }
 
 func run(pass *analysis.Pass) (any, error) {
-	inspect := func(node ast.Node) bool {
+	inspector := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+
+	nodeFilter := []ast.Node{
+		(*ast.FuncDecl)(nil),
+	}
+	inspector.Preorder(nodeFilter, func(node ast.Node) {
 		funcDecl, ok := node.(*ast.FuncDecl)
 
 		if !ok {
-			return true
+			return
 		}
 
 		var tmpmap = make(map[string]float64)
@@ -29,23 +36,20 @@ func run(pass *analysis.Pass) (any, error) {
 			firstParamName := params[i].Names[0].Name
 			firstParamType, ok := params[i].Type.(*ast.Ident)
 			if !ok {
-
-				fmt.Printf("What1")
-				return true
+				return
 			}
 
 			if i > 0 {
 				previousParamName := params[i-1].Names[0].Name
 				previousParamType, ok := params[i-1].Type.(*ast.Ident)
 				if !ok {
-					fmt.Printf("What3")
-					return true
+					return
 				}
 
 				if tmpmap[firstParamType.Name] > 0 {
 					pass.Reportf(node.Pos(), "param '%s' with type '%s' for function '%s' should be combined with param '%s' of type '%s'\n",
 						firstParamName, firstParamType.Name, funcDecl.Name.Name, previousParamName, previousParamType.Name)
-					return true
+					return
 				}
 				// reset the previous second param count to 0
 				tmpmap[previousParamType.Name] = 0
@@ -58,26 +62,20 @@ func run(pass *analysis.Pass) (any, error) {
 				secondParamName := params[i+1].Names[0].Name
 				secondParamType, ok := params[i+1].Type.(*ast.Ident)
 				if !ok {
-
-					fmt.Printf("What4")
-					return true
+					return
 				}
 
 				// fail if type already exist in map with count 1
 				if tmpmap[secondParamType.Name] > 0 {
 					pass.Reportf(node.Pos(), "param '%s' with type '%s' for function '%s' should be combined with param '%s' of type '%s'\n",
 						secondParamName, secondParamType.Name, funcDecl.Name.Name, firstParamName, firstParamType.Name)
-					return true
+					return
 				}
 				// if types for 1st and 2nd param != reset first param counter to 1 and set param counter for 2 param to 1
 				tmpmap[firstParamType.Name] = 0
 				tmpmap[secondParamType.Name] = 1
 			}
 		}
-		return true
-	}
-	for _, f := range pass.Files {
-		ast.Inspect(f, inspect)
-	}
+	})
 	return nil, nil
 }
---
+++ testdata/src/p/p.go
@@ -0,0 +1,9 @@
+package p
+
+func alternatingParams(a string, b bool, c string) {}
+
+func combinedParams1(a, b string, c bool) {}
+
+func combinedParams2(a bool, b, c string) {}
+
+func combinedParams3(a, b string, c bool, d, e string) {}
