b776fe7675acacad083ccc1376d5c7742f3d8d79
Jenny Ly <lycb@github.com>
2022-09-20 13:23:56 -0400

    add cases for previousparam

--- pkg/analyzer/analyzer.go
+++ pkg/analyzer/analyzer.go
@@ -1,6 +1,7 @@
 package analyzer
 
 import (
+	"fmt"
 	"go/ast"
 
 	"golang.org/x/tools/go/analysis"
@@ -28,28 +29,37 @@ func run(pass *analysis.Pass) (any, error) {
 			firstParamName := params[i].Names[0].Name
 			firstParamType, ok := params[i].Type.(*ast.Ident)
 			if !ok {
+
+				fmt.Printf("What1")
 				return true
 			}
 
-			if tmpmap[firstParamType.Name] > 0 {
-				return true
-			} else {
-				if i > 0 {
-					previousParam, ok := params[i-1].Type.(*ast.Ident)
-					if !ok {
-						return true
-					}
-					// reset the previous second param count to 0
-					tmpmap[previousParam.Name] = 0
+			if i > 0 {
+				previousParamName := params[i-1].Names[0].Name
+				previousParamType, ok := params[i-1].Type.(*ast.Ident)
+				if !ok {
+					fmt.Printf("What3")
+					return true
 				}
-				tmpmap[firstParamType.Name] = 1
+
+				if tmpmap[firstParamType.Name] > 0 {
+					pass.Reportf(node.Pos(), "param '%s' with type '%s' for function '%s' should be combined with param '%s' of type '%s'\n",
+						firstParamName, firstParamType.Name, funcDecl.Name.Name, previousParamName, previousParamType.Name)
+					return true
+				}
+				// reset the previous second param count to 0
+				tmpmap[previousParamType.Name] = 0
 			}
+
+			tmpmap[firstParamType.Name] = 1
 
 			// if there is a second param
 			if i+1 < len(params) {
 				secondParamName := params[i+1].Names[0].Name
 				secondParamType, ok := params[i+1].Type.(*ast.Ident)
 				if !ok {
+
+					fmt.Printf("What4")
 					return true
 				}
 
@@ -58,11 +68,10 @@ func run(pass *analysis.Pass) (any, error) {
 					pass.Reportf(node.Pos(), "param '%s' with type '%s' for function '%s' should be combined with param '%s' of type '%s'\n",
 						secondParamName, secondParamType.Name, funcDecl.Name.Name, firstParamName, firstParamType.Name)
 					return true
-				} else {
-					// if types for 1st and 2nd param != reset first param counter to 1 and set param counter for 2 param to 1
-					tmpmap[firstParamType.Name] = 0
-					tmpmap[secondParamType.Name] = 1
 				}
+				// if types for 1st and 2nd param != reset first param counter to 1 and set param counter for 2 param to 1
+				tmpmap[firstParamType.Name] = 0
+				tmpmap[secondParamType.Name] = 1
 			}
 		}
 		return true
@@ -70,7 +79,5 @@ func run(pass *analysis.Pass) (any, error) {
 	for _, f := range pass.Files {
 		ast.Inspect(f, inspect)
 	}
-
 	return nil, nil
-
 }
