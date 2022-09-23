ace5dfd4c58ea3d027bbac21a938c44812e9aaa7
Jenny Ly <lycb@github.com>
2022-09-23 16:33:56 -0400

    add test

--- testdata/src/p/p.go
+++ testdata/src/p/p.go
@@ -1,5 +1,11 @@
 package p
 
+type (
+	test struct {
+		Num int
+	}
+)
+
 func alternatingParams(a string, b bool, c string) {}
 
 func combinedParams1(a, b string, c bool) {}
@@ -8,6 +14,12 @@ func combinedParams2(a bool, b, c string) {}
 
 func combinedParams3(a, b string, c bool, d, e string) {}
 
+func combinedParams4(a bool, b, c string, d bool, e string) {}
+
 func returnAString(a, b string) string { return "" }
 
 func returnAString2(a bool) string { return "" }
+
+func (a *test) withStruct(b bool, c, d string, e bool, f string) (string, int) {
+	return "", 0
+}
